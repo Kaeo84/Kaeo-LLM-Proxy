0e788f774b365173ac8c6fa8c8ed4b7b8b5c32d4
Chad Gienau <168867618+Spideym84@users.noreply.github.com>
2026-09-05 18:37:41 -0500

    Fix VSIX package registration: hand-author .pkgdef (CLI CreatePkgDef cannot load net10.0) and bundle it in the VSIX; simplify package to rely on [ProvideToolWindow]

--- Kaeo VS Extension/VsPackage.cs
+++ Kaeo VS Extension/VsPackage.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Runtime.InteropServices;
-using System.ComponentModel.Design;
 using Microsoft.VisualStudio.Shell;
-using Microsoft.VisualStudio.Shell.Interop;
 using Task = System.Threading.Tasks.Task;
 using Kaeo.LlmProxy.VSExtension.ToolWindow;
 
@@ -10,31 +8,15 @@ namespace Kaeo.LlmProxy.VSExtension;
 
 [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
 [ProvideToolWindow(typeof(ToolWindowPaneImpl))]
-[ProvideMenuResource("Menus.ctmenu", 1)]
 [Guid(PackageGuidString)]
 public sealed class VsPackage : AsyncPackage
 {
     public const string PackageGuidString = "d1f6a6c2-0000-4a6d-9c2f-000000000000";
-    public const string CmdSetGuidString = "a1b2c3d4-0000-4a6d-9c2f-000000000002";
 
-    protected override async Task InitializeAsync(System.Threading.CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+    protected override Task InitializeAsync(System.Threading.CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
     {
-        await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
-
-        if (await this.GetServiceAsync(typeof(IMenuCommandService)) is OleMenuCommandService commandService)
-        {
-            var cmdId = new CommandID(new Guid(CmdSetGuidString), 0x0100);
-            var menuItem = new MenuCommand(ShowToolWindow, cmdId);
-            commandService.AddCommand(menuItem);
-        }
-    }
-
-    private void ShowToolWindow(object? sender, EventArgs e)
-    {
-        _ = this.JoinableTaskFactory.RunAsync(async () =>
-        {
-            await this.JoinableTaskFactory.SwitchToMainThreadAsync();
-            var window = await this.ShowToolWindowAsync(typeof(ToolWindowPaneImpl), 0, true, this.DisposalToken);
-        });
+        // The tool window is registered via [ProvideToolWindow] and appears under
+        // View > Other Windows > Kaeo Assistant.
+        return Task.CompletedTask;
     }
 }
